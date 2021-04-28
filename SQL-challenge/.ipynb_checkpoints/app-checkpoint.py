d60ac7740c1a04797a69b30ded245f47625486f7
Streds <laurenstredler@MacBook-Pro.local>
2021-04-27 20:40:20 -0500

    app

--- SQL-challenge/.ipynb_checkpoints/app-checkpoint.py
+++ SQL-challenge/.ipynb_checkpoints/app-checkpoint.py
@@ -1,23 +1,75 @@
-# 1. import Flask
-from flask import Flask
+#import dependencies
+import numpy as np
+import datetime as dt
+import pandas as pd
+import numpy as np
 
-# 2. Create an app, being sure to pass __name__
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+#generate the engine to the corrext sqlite file 
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+#Uses the automap_base()
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurements = Base.classes.measurements
+Station=Base.classes.station
+
+#create and bind the session between the python app and database 
+session=Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
+#################################################
+# Flask Routes
+#################################################
 
-# 3. Define what to do when a user hits the index route
 @app.route("/")
-def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+
+    )
+##############################################
 
 
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+@app.route("/api/v1.0/precipitation")
+def stations():
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+##############################################
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    """Return a list of stations"""
+    # Query measurements 
+    station_query = session.query(Stations.name, Station.station)
+    stations=pd.read_sql(stations_query.statement, stations_query.session.bind)
+    return jsonify(stations.to_dict())
+
+##############################################
+@app.route("/api/v1.0/tobs")
+def tobs():
